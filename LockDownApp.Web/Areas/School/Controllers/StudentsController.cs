fef8a0a56fc06cb5c42fa5109dde83cfb6b0b90e
Aminur <aminur6264@gmail.com>
2021-01-02 21:33:13 +0600

    Bug fix

--- LockDownApp.Web/Areas/School/Controllers/StudentsController.cs
+++ LockDownApp.Web/Areas/School/Controllers/StudentsController.cs
@@ -67,8 +67,8 @@ namespace LockDownApp.Web.Areas.School.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,StudentName,ContactNo")] StudentViewModel studentViewModel)
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create( StudentViewModel studentViewModel)
         {
             if (ModelState.IsValid)
             {
